0104250d11a312863c653a614b88bd8a2590452b
Ivan <vans5000@yandex.ru>
2016-11-08 23:32:07 +0500

    Added visual tank turning.

--- src/com/company/core/models/Tank.java
+++ src/com/company/core/models/Tank.java
@@ -36,7 +36,26 @@ public class Tank {
 	public void draw(Graphics g) {
         // Отрисовка танка, вначале установим цвет кисти
 		g.setColor(color);
-		g.fillRect(x - width / 2, y - height / 2, width, height);
-		g.fillRect(x - width/8, y - height, width/5, height / 2);
+
+		switch(curDirection){
+			case UP:
+				g.fillRect(x - width / 2, y - height / 2, width, height);
+				g.fillRect(x - width/8, y - height, width/5, height / 2);
+				break;
+			case DOWN:
+				g.fillRect(x - width / 2, y - height / 2, width, height);
+				g.fillRect(x - width/8, y + height / 2, width/5, height/2);
+				break;
+			case LEFT:
+				g.fillRect(x - height / 2, y - (width / 2), height, width);
+				g.fillRect(x - height, y - width/8, height/2, width/5);
+				break;
+			case RIGHT:
+				g.fillRect(x - height / 2, y - width / 2, height, width);
+				g.fillRect(x + height / 2, y - width/8, height/2, width/5);
+				break;
+		}
+
+
 	}
 }
